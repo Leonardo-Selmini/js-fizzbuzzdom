a937c0c9fb2814f83ef3f27d0cdc14277f642355
Leonardo.Selmini <selminileonardo@gmail.com>
2021-11-09 18:58:47 +0100

    js - add conditions

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ for (let i = 1; i <= 100; i++) {
   // controllo che funzioni
   console.log(i);
   // itero text
-  text = text + `<div class="box">${i}</div>`;
+  if (i % 15 == 0) {
+    text += `<div class="box fizz-buzz">BuzzFizz</div>`;
+  } else if (i % 5 == 0) {
+    text += `<div class="box buzz">Buzz</div>`;
+  } else if (i % 3 == 0) {
+    text += `<div class="box fizz">Fizz</div>`;
+  } else {
+    text += `<div class="box">${i}</div>`;
+  }
 }
 // seleziono il mio container e ci printo dentro i box
 document.getElementById('box-container').innerHTML = text

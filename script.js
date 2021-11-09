d09acf510dfc4b573f19800ab418e71c51ba690f
Leonardo.Selmini <selminileonardo@gmail.com>
2021-11-09 16:31:13 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let text = '';
+// loop per stampare i numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+  // controllo che funzioni
+  console.log(i);
+  // itero text
+  text = text + `<div class="box">${i}</div>`;
+}
+// seleziono il mio container e ci printo dentro i box
+document.getElementById('box-container').innerHTML = text
